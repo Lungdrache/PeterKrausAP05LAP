3304f8fd469c7cc75f51a74d0280e64fa4f6ef45
Peter Kraus <lungdrache@yahoo.de>
2021-10-18 14:03:34 +0200

    - added MailManager - added SecureManager - added LoginView - added RegistraturView

---
+++ PeterKrausAP05LAP/Controllers/AccountController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using PeterKrausAP05LAP.Models;
+
+namespace PeterKrausAP05LAP.Controllers
+{
+    public class AccountController : Controller
+    {
+        public static StockGamesDatabaseEntities context = new StockGamesDatabaseEntities();
+
+        [HttpGet]
+        [ActionName("Login")]
+        public ActionResult LoginPageGet()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Login")]
+        public ActionResult LoginPagePost(string email, string password)
+        {
+            
+
+            return View();
+        }
+
+        [HttpGet]
+        [ActionName("Registratur")]
+        public ActionResult RegistraturGet()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        [ActionName("Registratur")]
+        public ActionResult RegistraturPost(string title,string adress, string fname, string lname, string email, string password, int PLZ, string Ort)
+        {
+            Customer newCustomer = new Customer(title,fname,lname,email,adress,PLZ.ToString(),Ort,password);
+
+            // mail Sending doesn't work atm
+            // MailManager.SendEmail($"<br/><br/>Hello {fname} {lname}<br/><br/> You succefully registered to our Website!", $"Welcome {lname} to Stockgames",email);
+
+            context.Customer.Add(newCustomer);
+            context.SaveChanges();
+
+            return View();
+        }
+
+        // GET: Account/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Account/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Account/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Account/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Account/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Account/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Account/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ PeterKrausAP05LAP/Models/MailManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace PeterKrausAP05LAP.Models
+{
+    public class MailManager
+    {
+        //TODO: MakeItWork
+        public static bool SendEmail(string htmlBody,string subject, string reciever)
+        {
+            {
+                MailMessage message = new MailMessage();
+
+                SmtpClient smtp = new SmtpClient();
+
+                message.From = new MailAddress("noreplystockgames.benny@gmail.com");
+                message.To.Add(new MailAddress(reciever));
+                message.Subject = subject;
+                message.IsBodyHtml = true; //to make message body as html  
+                message.Body = htmlBody;
+
+
+                smtp.Port = 587;
+                smtp.Host = "smtp.gmail.com"; //for gmail host  
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                #region Credentials
+                smtp.Credentials = new NetworkCredential(message.From.Address, "Drachenfeuer10.");
+                #endregion
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+                smtp.Send(message);
+
+                return true;
+            }
+
+            //try
+            //{
+            //    MailMessage message = new MailMessage();
+
+            //    SmtpClient smtp = new SmtpClient();
+
+            //    message.From = new MailAddress("noreplystockgames.benny@gmail.com");
+            //    message.To.Add(new MailAddress(reciever));
+            //    message.Subject = subject;
+            //    message.IsBodyHtml = true; //to make message body as html  
+            //    message.Body = htmlBody;
+                
+                
+            //    smtp.Port = 587;
+            //    smtp.Host = "smtp.gmail.com"; //for gmail host  
+            //    smtp.EnableSsl = true;
+            //    smtp.UseDefaultCredentials = false;
+            //    #region Credentials
+            //    smtp.Credentials = new NetworkCredential(message.From.Address, "Drachenfeuer10.");
+            //    #endregion
+            //    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+            //    smtp.Send(message);
+
+            //    return true;
+            //}
+            //catch (Exception exp) 
+            //{ 
+            //    return false; 
+            //}
+        }
+    }
+}
